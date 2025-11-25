772382caf987be8d87dfca8dec80af2e779ceafa
Yasaka0601 <www.gtr.0601@icloud.com>
2025-11-25 20:18:19 +0900

    add:トップページ画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do #ルーティングを定義する宣言
+  root "homes#top" #ルートパスにアクセスされたら、HomesControllerのtopアクションを呼ぶ。
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  #アプリが動いているかhealth_checkするルート。
 
-  # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # PWA用のservice worker（ブラウザ側で動くJS）を返すルート。PWAとは、簡単に言うと「アプリっぽく動かす仕組み」
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # PWA用の設定ファイル（アプリアイコンや名前などの情報）を返すルート。
 end
