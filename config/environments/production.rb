a78524377c147f883cb64d7f622de90046200fc1
Yasaka0601 <www.gtr.0601@icloud.com>
2025-12-02 21:34:05 +0900

    デプロイエラー修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,11 +100,14 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = Settings.default_url_options.to_h
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
 
   # Gmail を使用する設定-------ここから-------
+  config.action_mailer.default_url_options = {
+  host:     'fraud-prevention-l35r.onrender.com',
+  protocol: 'https'
+  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
