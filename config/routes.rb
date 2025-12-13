1e64a13d338ece08b00ba2d6eb70017b182b2e65
Yasaka0601 <www.gtr.0601@icloud.com>
2025-12-13 16:31:52 +0900

    コース一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,19 @@ Rails.application.routes.draw do
       resources :invitations, only: %i[ show new create edit update ]
   end
 
+  # categories に courses をネストさせたいだけなので、only 指定はしない。
+  resources :categories, only: [] do
+    resources :courses, only: [:index]
+  end
+
+  # クイズ画面のルーティング。 quiz/courses/:id/play というパスを生成している。
+  resources :courses, only: [] do
+    member do
+      get :play, to: 'plays#show'
+    end
+  end
+
+
   ##### アプリが動いているかhealth_checkするルート。 #####
   get "up" => "rails/health#show", as: :rails_health_check
 
