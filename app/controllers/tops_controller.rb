3cbc71a9c2813576c656bdd9f8e0007d094ef96e
Yasaka0601 <www.gtr.0601@icloud.com>
2025-12-25 21:38:45 +0900

    ログイン済の場合、homeへ遷移

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,7 @@ class TopsController < ApplicationController
   # top アクションだけは、ログインしてなくてもアクセスできる。
   skip_before_action :authenticate_user!, only: :top
 
-  def top; end
+  def top
+    redirect_to home_path if user_signed_in?
+  end
 end
