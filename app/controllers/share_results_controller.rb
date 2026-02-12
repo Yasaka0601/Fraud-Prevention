321b42c6495ebaf1c953b67cb57002555c2e802d
Yasaka0601 <www.gtr.0601@icloud.com>
2026-02-12 12:11:10 +0900

    リンク失効ページ実装

--- app/controllers/share_results_controller.rb
+++ app/controllers/share_results_controller.rb
@@ -20,20 +20,24 @@ class ShareResultsController < ApplicationController
   private
 
   # 共有URLに含まれる token から、対応する成績データを取り出すメソッド
+  # find_signed で params で受け取ったトークンを検証。問題なければ return でメソッド終了。
   def set_course_result_from_token
     @course_result = CourseResult.includes(course: :category)
-                                 .find_signed(params[:token], purpose: :share_result)
+                                  .find_signed(params[:token], purpose: :share_result)
     return if @course_result.present?
 
+    # トークン検証で問題がある場合。
     render_expired_page
   end
 
+  # リンク有効期限切れ、（30日）もしくは、成績が消去されていた場合
   def render_expired_page
     helpers.assign_meta_tags(
       title: "共有リンクの有効期限が切れました",
       description: "共有された成績リンクは有効期限切れ、または無効です。",
       url: request.original_url
     )
+    # expiredページを表示。status: :gone でステータス 410
     render :expired, status: :gone
   end
 end
