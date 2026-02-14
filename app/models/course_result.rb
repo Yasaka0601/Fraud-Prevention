8b0398c0c9e857e4f76d8023cb3ce5b11d524075
Yasaka0601 <www.gtr.0601@icloud.com>
2026-02-14 16:28:09 +0900

    コメント追加。

--- app/models/course_result.rb
+++ app/models/course_result.rb
@@ -14,36 +14,45 @@ class CourseResult < ApplicationRecord
     # ポイントをカウントする変数。
     get_point = 0
 
+    ##### メソッドのベースとなるオブジェクトを作成。#####
     # 受け取った引数や、上記の変数で CourseResult モデルのオブジェクトを生成。
     course_result = CourseResult.create!(
       user: user,
       course: course,
       correct_count: 0,
       total_questions: total_questions,
-      started_at: nil,
+      started_at: nil, # started_at はいらない。
       finished_at: Time.current
     )
+
+    ##### クイズ、ユーザー、回答の選択肢を保存する。#####
     # each_with_index は 要素と何番目か (index) を一緒にループさせる。
     quiz_ids.each_with_index do |quiz_id, index|
-      # quiz_id を使って DB からクイズを取得し、代入している。
+      ##### クイズを取得し、1問分の回答履歴（quiz_history）を作成 #####
+      # quiz_id を使って DB からクイズを取得している。
       quiz = Quiz.find(quiz_id)
-      # ユーザーの選択肢を整数のid配列にして selected_ids に代入。
-      selected_ids = Array(answers[index]).map(&:to_i).uniq
-      # コースの中の一問分の履歴を生成。
+
+      # クイズ１問分の履歴を生成。
       quiz_history = course_result.quiz_histories.create!(
         user: user,
         quiz: quiz
       )
 
+      ##### ユーザーの回答を記録する #####
+      # ユーザーの回答を整数のid配列にして selected_ids に代入。
+      selected_ids = Array(answers[index]).map(&:to_i).uniq.sort
+
+      # 選択した choice_id を quiz_history_choices として保存
       selected_ids.each do |choice_id|
-        # ユーザーが選んだ複数の選択肢（choice_id 達）を、 quiz_history_choices に全部保存している
         quiz_history.quiz_history_choices.create!(choice_id: choice_id)
       end
 
+      ##### 答え合わせ #####
       # 正解の選択肢を取得し、id を抽出し、ソートして代入。
       correct_ids = quiz.choices.where(is_correct: true).pluck(:id).sort
+
       # 答え合わせをしている。正解であれば true
-      if selected_ids.sort == correct_ids
+      if selected_ids == correct_ids
         correct_count += 1 #正解数に 1 を追加。
         get_point += quiz.give_point.to_i #正解したクイズのポイントを追加。
       end
@@ -51,13 +60,14 @@ class CourseResult < ApplicationRecord
     # 正解数を最新の状態にアップデート
     course_result.update!(correct_count: correct_count)
 
+    ##### ポイント計算 #####
     # ユーザーの total_point にget_point を加算。positive? でポイントが 1 以上か判定。
     if get_point.positive?
       # increment! は ActiveRecord の数値カラムを加算し、保存するメソッド。
       user.increment!(:total_point, get_point)
     end
 
-    # 成績履歴を20件に制限（21件目以降は古いのを削除）
+    # 成績履歴を20件に制限（21件目以降は古いのを削除、21件目以降が excess に代入される。
     excess = user.course_results.order(created_at: :desc).offset(20)
     excess.delete_all
 
