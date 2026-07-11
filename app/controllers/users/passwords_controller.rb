e1cf5faeb021abb9186d2bef8e94231ae8dcf7a6
Yasaka0601 <www.gtr.0601@icloud.com>
2026-07-11 15:08:45 +0900

    バリデーションエラーのフラッシュメッセージ化

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,2 +1,15 @@
 class Users::PasswordsController < Devise::PasswordsController
+  def create
+    super { |resource| set_error_flash(resource) }
+  end
+
+  def update
+    super { |resource| set_error_flash(resource) }
+  end
+
+  private
+
+  def set_error_flash(resource)
+    flash.now[:alert] = resource.errors.full_messages.join("、") if resource.errors.any?
+  end
 end
